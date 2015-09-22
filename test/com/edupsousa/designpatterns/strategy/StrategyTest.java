d9b28f6f87b54406746d380a53fa0db4d06c2b7b
Riany Oliveira <rianyoliveira@gmail.com>
2015-09-22 20:07:32 -0300

    Criada classe para instanciar a classe antes de realizar o teste.

--- test/com/edupsousa/designpatterns/strategy/StrategyTest.java
+++ test/com/edupsousa/designpatterns/strategy/StrategyTest.java
@@ -2,13 +2,20 @@ package com.edupsousa.designpatterns.strategy;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StrategyTest {
-
+	
+	ClasseComStrategy objeto; 
+	
+	@Before
+	public void instaciarClasse(){
+		objeto = new ClasseComStrategy();
+	}
+	
 	@Test
 	public void testarPadraoStrategy() {
-		ClasseComStrategy objeto = new ClasseComStrategy();
 		
 		objeto.defineStrategy(new StrategyConcretaA());
 		assertEquals("Ação realizada por StrategyConcretaA", objeto.usaStrategyParaFazerAlgo());
